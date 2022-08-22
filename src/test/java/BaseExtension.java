885f4b87d9e7ece4d8cf2fee807f88414e2dd3b7
Patryk <pszindler@sii.pl>
2022-08-22 21:33:53 +0200

    initial commit

--- src/test/java/BaseExtension.java
+++ src/test/java/BaseExtension.java
@@ -2,6 +2,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInstance;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
@@ -9,13 +10,14 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.logging.Level;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 abstract class BaseExtension {
 
     static protected WebDriver driver;
     ChromeOptions chromeOptions = new ChromeOptions();
 
     @BeforeAll
-    static void beforeAll() {
+     void beforeAll() {
         WebDriverManager.chromedriver().setup();
         java.util.logging.Logger.getLogger("org.openqa.selenium").setLevel(Level.OFF);
         System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
